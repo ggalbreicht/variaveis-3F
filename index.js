e418214d411cd12de90980ced0922edbdb2beae4
ggalbreicht <163944744+ggalbreicht@users.noreply.github.com>
2024-09-09 23:02:39 +0000

    09/09/24

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const anoNascimento = 2007;
 let anoAtual = 2024;
 const nomeCompleto = "Geovana Albreicht";
 
-console.log("Meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos");
+console.log("Meu nome é " + nomeCompleto + " nascida no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos");
 
 anoAtual = anoAtual + 1;
 console.log(anoAtual);
@@ -14,18 +14,28 @@ console.log(idade);
 
 //Agência de viagens Hawaii Paris Nova York Rio de Janeiro Tóquio
 const listaDeDestinos = new Array(
-  "Hawaii",
-  "Paris",
-  "Nova York",
-  "Rio de Janeiro",
-  "Tóquio"
+  " Hawaii",
+  " Paris",
+  " Nova York",
+  " Rio de Janeiro",
+  " Tóquio "
 );
 
 console.log(listaDeDestinos);
 console.log(listaDeDestinos[3]);
-listaDeDestinos.push("Coronel Vivida");
-listaDeDestinos.push("Curitiba");
-listaDeDestinos.push("Madrid");
-listaDeDestinos.push("Berlin");
+listaDeDestinos.push(" Coronel Vivida");
+listaDeDestinos.push(" Curitiba");
+listaDeDestinos.push(" Madrid");
+listaDeDestinos.push(" Berlin");
 console.log(listaDeDestinos);
 
+
+const idadeComprador = 18;
+
+if(idadeComprador >= 18) {
+  console.log("Comprador maior de idade");
+  console.log("destinos disponíveis: " + listaDeDestinos);
+} else {
+  console.log("Não foi posível concluir a compra");
+  console.log("Vocẽ é menor de idade");
+}
